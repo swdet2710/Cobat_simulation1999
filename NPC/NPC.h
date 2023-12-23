74d533f5e6c3bc3d6c5fcf69528d252496e972ce
Shui Tan <137278348+swdet2710@users.noreply.github.com>
2023-12-24 00:00:19 +0800

    Update and rename NPC.h to NPC/NPC.h

--- NPC/NPC.h
+++ NPC/NPC.h
@@ -1,8 +1,6 @@
-#pragma once
-#include "card.h"
+
 #include "head.h"
-#include <fstream>
-#include <array>
+
 class NPC
 {
 public:
@@ -12,23 +10,42 @@ public:
 	buff shield; //护盾
 	property init_npc_propertys; //初始角色属性
 	property npc_propertys; //每回合时角色属性
-	buff *bufflist_timepass; //回合buff
-	buff*bufflist_workpass; //消耗buff
-	buff* debufflist_timepass; //回合debuff
-	buff* debufflist_workpass; //消耗debuff
-	std::array<CardBase*, 3> skills; // 技能组
-
+	buff *bufflist_timepass = NULL; //回合buff
+	//buff*bufflist_workpass; //消耗buff
+	buff* bufflist_attack = NULL; //受到攻击时buff
+	//buff* debufflist_timepass; //回合debuff
+	//buff* debufflist_workpass; //消耗debuff
+	//buff* debufflist_attack; //受到攻击时debuff
 	NPC(string names) {
 		name = names;
-		bufflist_timepass = new buff[100];
-		bufflist_workpass = new buff[100];
-		debufflist_timepass = new buff[100];
-		debufflist_workpass = new buff[100];
+	}
+	~NPC() {
+		buff* next;
+		if(bufflist_timepass!=NULL)
+			for (buff* i = bufflist_timepass; i != NULL; i = next)
+			{
+				next = i->next;
+				delete i;
+			}
+		if (bufflist_attack != NULL)
+			for (buff* i = bufflist_attack; i != NULL; i = next)
+			{
+				next = i->next;
+				delete i;
+			}
 	}
 
 	int combat_readiness(); //游戏开始初始化
 
 	int load_property(); //从文件载入角色信息
+
+	void buff_to_property(); //刷新属性
+
+	void by_attack(NPC self,NPC opponent); //受到攻击时触发
+
+	void by_timepass(); //回合结束时触发
+
+	void by_timebegin(); //回合开始时判定
 
 	float attacks(float attack,property&opponent,int attack_property); //攻防项计算
 
@@ -39,8 +56,6 @@ public:
 	float critical_check(property& opponent); //攻击暴击运算
 
 	float health_critical_check(); //治疗暴击运算
-
-	void set_skills(std::array<CardBase*, 3> _skills) { skills = _skills; }
 
 };
 
