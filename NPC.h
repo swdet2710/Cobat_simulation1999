cdb2b6cc71e73c75cfc8d7b5901cdbc0642265ad
Shui Tan <137278348+swdet2710@users.noreply.github.com>
2023-12-24 11:42:20 +0800

    Update and rename NPC/NPC.h to NPC.h

--- NPC.h
+++ NPC.h
@@ -3,21 +3,13 @@
 
 class NPC
 {
-public:
+private:
 	string name;
-	int brightness; //灵光
-	float live; //剩余生命
-	buff shield; //护盾
 	property init_npc_propertys; //初始角色属性
-	property npc_propertys; //每回合时角色属性
-	buff *bufflist_timepass = NULL; //回合buff
-	//buff*bufflist_workpass; //消耗buff
-	buff* bufflist_attack = NULL; //受到攻击时buff
-	//buff* debufflist_timepass; //回合debuff
-	//buff* debufflist_workpass; //消耗debuff
-	//buff* debufflist_attack; //受到攻击时debuff
+	int load_property(); //从文件载入角色信息
 	NPC(string names) {
 		name = names;
+		load_property();
 	}
 	~NPC() {
 		buff* next;
@@ -33,27 +25,41 @@ public:
 				next = i->next;
 				delete i;
 			}
+		if (bufflist_byattack != NULL)
+			for (buff* i = bufflist_byattack; i != NULL; i = next)
+			{
+				next = i->next;
+				delete i;
+			}
 	}
+	
+	void buff_attack(); //攻击时触发
+	float attacks(float attack, property& opponent, int attack_property); //攻防项计算
+	float critical_check(property& opponent); //攻击暴击运算
+public:
+	
+	int brightness; //灵光
+	float live; //剩余生命
+	buff shield; //护盾
+	
+	property npc_propertys; //每回合时角色属性
+	buff *bufflist_timepass = NULL; //回合buff
+	buff* bufflist_byattack = NULL; //受到攻击后buff
+	buff* bufflist_attack = NULL; //攻击后buff
 
 	int combat_readiness(); //游戏开始初始化
 
-	int load_property(); //从文件载入角色信息
-
 	void buff_to_property(); //刷新属性
 
-	void by_attack(NPC self,NPC opponent); //受到攻击时触发
+	void use_to_attack(NPC self, NPC* opponent, void (*p)(), int sum_of_opponent = 1); //攻击时调用
 
 	void by_timepass(); //回合结束时触发
-
+	void by_attack(); //受到攻击时触发
 	void by_timebegin(); //回合开始时判定
-
-	float attacks(float attack,property&opponent,int attack_property); //攻防项计算
 
 	float health(float attack,float scale); //未暴击的治疗计算（攻击或损失血量，倍率）
 
 	float atack(float attack, float scale,property& opponent,int attack_property,int attack_way); //攻击计算（攻击，倍率,对方面板,法术属性，释放属性）
-
-	float critical_check(property& opponent); //攻击暴击运算
 
 	float health_critical_check(); //治疗暴击运算
 
